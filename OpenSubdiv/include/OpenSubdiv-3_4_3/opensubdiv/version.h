bc75d0e22041473310a3b2d6154aa659424b7948
root <root@localhost.localdomain>
2026-06-23 14:35:45 +0000

    OpenSubdiv: replace symlinks with copied include files

--- OpenSubdiv/include/OpenSubdiv-3_4_3/opensubdiv/version.h
+++ OpenSubdiv/include/OpenSubdiv-3_4_3/opensubdiv/version.h
@@ -1,1 +1,28 @@
-/tmp/android-blender-libs/OpenSubdiv/include/opensubdiv/version.h
+//
+//   Copyright 2013 Pixar
+//
+//   Licensed under the terms set forth in the LICENSE.txt file available at
+//   https://opensubdiv.org/license.
+//
+
+#ifndef OPENSUBDIV3_VERSION_H
+#define OPENSUBDIV3_VERSION_H
+
+#define OPENSUBDIV_VERSION v3_7_0
+
+#define OPENSUBDIV_VERSION_NUMBER 30700
+
+#define OPENSUBDIV_VERSION_MAJOR 3
+#define OPENSUBDIV_VERSION_MINOR 7
+#define OPENSUBDIV_VERSION_PATCH 0
+
+namespace OpenSubdiv {
+namespace OPENSUBDIV_VERSION {
+
+
+} // end namespace OPENSUBDIV_VERSION
+using namespace OPENSUBDIV_VERSION;
+
+} // end namespace OpenSubdiv
+
+#endif /* OPENSUBDIV3_VERSION_H */
