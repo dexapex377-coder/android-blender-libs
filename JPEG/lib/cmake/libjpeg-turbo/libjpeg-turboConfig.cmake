aef0ea665527bf5318c3624aa10439aec028ed7c
root <root@localhost.localdomain>
2026-06-23 00:40:38 +0000

    Fix: OpenCOLLADA CRLF+PCRE block, OIDN rm namespace, JPEG libjpeg-turbo cmake config, OpenPGL CMAKE_FIND_ROOT_PATH, OpenVDB CMAKE_FIND_ROOT_PATH+Tbb_INCLUDE_DIR

---
+++ JPEG/lib/cmake/libjpeg-turbo/libjpeg-turboConfig.cmake
@@ -0,0 +1,19 @@
+set(PACKAGE_VERSION "2.1.5")
+if(PACKAGE_FIND_VERSION)
+  if(PACKAGE_FIND_VERSION VERSION_LESS_EQUAL PACKAGE_VERSION)
+    set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  else()
+    set(PACKAGE_VERSION_COMPATIBLE FALSE)
+  endif()
+endif()
+
+if(NOT TARGET libjpeg-turbo::jpeg)
+  add_library(libjpeg-turbo::jpeg STATIC IMPORTED)
+  set_target_properties(libjpeg-turbo::jpeg PROPERTIES
+    IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/libjpeg.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../../include"
+  )
+endif()
+set(libjpeg-turbo_FOUND TRUE)
+set(libjpeg-turbo_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../include")
+set(libjpeg-turbo_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/libjpeg.a")
