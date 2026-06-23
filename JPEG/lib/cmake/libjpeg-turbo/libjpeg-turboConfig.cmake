a7f94d8d53f27c8ea6e16f8a5ba1346183f6874e
root <root@localhost.localdomain>
2026-06-23 01:49:42 +0000

    Fix libjpeg-turbo cmake config relative path (4->3 levels)

--- JPEG/lib/cmake/libjpeg-turbo/libjpeg-turboConfig.cmake
+++ JPEG/lib/cmake/libjpeg-turbo/libjpeg-turboConfig.cmake
@@ -10,10 +10,10 @@ endif()
 if(NOT TARGET libjpeg-turbo::jpeg)
   add_library(libjpeg-turbo::jpeg STATIC IMPORTED)
   set_target_properties(libjpeg-turbo::jpeg PROPERTIES
-    IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/libjpeg.a"
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../../include"
+    IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../lib/libjpeg.a"
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../include"
   )
 endif()
 set(libjpeg-turbo_FOUND TRUE)
-set(libjpeg-turbo_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../../include")
-set(libjpeg-turbo_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../../lib/libjpeg.a")
+set(libjpeg-turbo_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../include")
+set(libjpeg-turbo_LIBRARY "${CMAKE_CURRENT_LIST_DIR}/../../../lib/libjpeg.a")
